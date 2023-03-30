6e6eb7d9d185dfe908b58759021845500228f9e7
Isabella Santiag <isabella.-.bela@hotmail.com>
2023-03-30 14:44:26 -0300

    test: ensure SignUpController returns an error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,5 +14,22 @@ describe('SignUp Controller', () => {
         
         const httResponse = sut.handle(httpRequest)
         expect(httResponse.statusCode).toBe(400)
+        expect(httResponse.body).toEqual(new Error('Missing param: name'))
+    })
+
+    test('Should return 400 if no name is provided', () => {
+        //sut- system under test (class to test)
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        
+        const httResponse = sut.handle(httpRequest)
+        expect(httResponse.statusCode).toBe(400)
+        expect(httResponse.body).toEqual(new Error('Missing param: email'))
     })
 })
